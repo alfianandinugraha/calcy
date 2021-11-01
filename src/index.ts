bc8e6c3df83058c56bfb3a30051cadec2567892c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-01 12:47:42 +0700

    feat(typescript): init typescript files, scripts, and config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Add two number
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @public
+ */
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+export { add };

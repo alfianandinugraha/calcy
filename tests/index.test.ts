ed265c1c3cddb81f06f7df87567e7bf1818c4ec3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-01 13:32:41 +0700

    test(case): add test case for 'substract' function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { add } from "../src";
+import { add, substract } from "../src";
 
 describe("Test 'add' function", () => {
   test("add of 2 and 8", () => {
@@ -11,5 +11,19 @@ describe("Test 'add' function", () => {
 
   test("add of 3 and 5", () => {
     expect(add(3, 5)).toBe(8);
+  });
+});
+
+describe("Test 'subtract' function", () => {
+  test("subtract of 2 and 2", () => {
+    expect(substract(2, 2)).toBe(0);
+  });
+
+  test("subtract of 5 and 3", () => {
+    expect(substract(5, 3)).toBe(2);
+  });
+
+  test("subtract of 4 and 6", () => {
+    expect(substract(4, 6)).toBe(-2);
   });
 });

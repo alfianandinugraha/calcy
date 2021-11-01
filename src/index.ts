74eed658af9d180c04bbefdc0e716827c3ae7117
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-01 12:59:28 +0700

    feat(calc): add substract, times, and divide

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,38 @@ const add = (a: number, b: number) => {
   return a + b;
 };
 
-export { add };
+/**
+ * Substract two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @public
+ */
+const substract = (a: number, b: number) => {
+  return a - b;
+};
+
+/**
+ * Times two number
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @public
+ */
+const times = (a: number, b: number) => {
+  return a * b;
+};
+
+/**
+ * Divide two numbers
+ *
+ * @param a - fisrt number
+ * @param b - second number
+ */
+const divide = (a: number, b: number) => {
+  return a / b;
+};
+
+export { add, substract, times, divide };

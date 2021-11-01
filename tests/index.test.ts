a139a1bb643bd9b7e212e7b6a55ccaddf2486e07
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-01 13:22:24 +0700

    test(case): add test case for "add" function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,4 +4,12 @@ describe("Test 'add' function", () => {
   test("add of 2 and 8", () => {
     expect(add(2, 8)).toBe(10);
   });
+
+  test("add of 2.5 and 2.5", () => {
+    expect(add(2.5, 2.5)).toBe(5);
+  });
+
+  test("add of 3 and 5", () => {
+    expect(add(3, 5)).toBe(8);
+  });
 });
